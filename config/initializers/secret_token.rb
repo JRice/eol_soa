9c60abff19af312c6dc9c8d743802c36da3c6858
JRice <jrice@eol.org>
2012-08-29 05:30:07 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EolSoa::Application.config.secret_token = 'af59f752c68b5a5fa236b1070e60f88a7bd5082ff8a91db24600fb09754e23138979d0096556a9ec1acf1f2eba352dc5652e1201e417983742e1a4f14af82e17'
